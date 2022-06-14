091207a561e8fbb4fc1f66e5966dc1b5e753a909
cyberbex <cyberbex@gmail.com>
2022-06-13 23:45:37 -0300

    aaaaaa

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask_restful import Api
 from resources.hotel import Hoteis, Hotel
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.sqlite3'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 
